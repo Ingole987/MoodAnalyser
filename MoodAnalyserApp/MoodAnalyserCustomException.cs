3303b1fa108c29b721da39087d519a2f010b293f
Ingole987 <pratik.ingole98@gmail.com>
2022-02-02 22:03:28 +0530

    added : ExceptionHandling And CustomException

---
+++ MoodAnalyserApp/MoodAnalyserCustomException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserApp
+{
+    public class MoodAnalyserCustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            NULL_MESSAGE, EMPTY_MESSAGE,
+            NO_SUCH_FILES, NO_SUCH_METHOD,
+            NO_SUCH_CLASS, OBJECT_CREATION_ISSUE
+        }
+        private readonly ExceptionType type;
+        public MoodAnalyserCustomException(ExceptionType Type, string message) : base(message)
+        {
+            this.type = Type;
+        }
+    }
+}
